5a4c7f7a242dcefe7fd5ae2629e04c74c475d057
Ryohei Ikegami <iofg2100@gmail.com>
2020-05-19 18:39:12 +0900

    generate anchors

--- examples/electron-mediapipe-face/renderer.ts
+++ examples/electron-mediapipe-face/renderer.ts
@@ -21,15 +21,45 @@ function sigmoid(x: number) {
   return 1 / (1 + Math.exp(-x));
 }
 
+function generateAnchors(width: number, height: number): [number, number][] {
+  const outputSpec = {
+    strides: [8, 16] as const,
+    anchors: [2, 6] as const,
+  };
+
+  const anchors: [number, number][] = [];
+  for (let i = 0; i < outputSpec.strides.length; i++) {
+    const stride = outputSpec.strides[i];
+    const gridRows = Math.floor((height + stride - 1) / stride);
+    const gridCols = Math.floor((width + stride - 1) / stride);
+    const anchorsNum = outputSpec.anchors[i];
+
+    for (let gridY = 0; gridY < gridRows; gridY++) {
+      const anchorY = stride * (gridY + 0.5);
+
+      for (let gridX = 0; gridX < gridCols; gridX++) {
+        const anchorX = stride * (gridX + 0.5);
+        for (let n = 0; n < anchorsNum; n++) {
+          anchors.push([anchorX, anchorY]);
+        }
+      }
+    }
+  }
+
+  return anchors;
+}
+
 class FaceDetector {
+  private inputSize = 128;
   private inputCanvas: HTMLCanvasElement;
   private inputContext: CanvasRenderingContext2D;
   private interpreter: Interpreter;
+  private anchors = generateAnchors(this.inputSize, this.inputSize);
 
   constructor() {
     this.inputCanvas = document.createElement("canvas");
-    this.inputCanvas.width = 128;
-    this.inputCanvas.height = 128;
+    this.inputCanvas.width = this.inputSize;
+    this.inputCanvas.height = this.inputSize;
     this.inputContext = this.inputCanvas.getContext("2d")!;
 
     const faceModelPath = path.resolve(
@@ -41,13 +71,7 @@ class FaceDetector {
   }
 
   detect(input: HTMLVideoElement) {
-    this.inputContext.drawImage(
-      input,
-      0,
-      0,
-      this.inputCanvas.width,
-      this.inputCanvas.height
-    );
+    this.inputContext.drawImage(input, 0, 0, this.inputSize, this.inputSize);
     const rgbFloat = canvasToRGBFloat(this.inputContext);
 
     this.interpreter.inputs[0].copyFrom(rgbFloat);
