1bc2c4c65e12540c762d6ed25c443fb9721ca4c1
Ryohei Ikegami <iofg2100@gmail.com>
2020-05-19 14:47:03 +0900

    Start video

--- examples/electron-mediapipe-face/main.ts
+++ examples/electron-mediapipe-face/main.ts
@@ -4,6 +4,9 @@ function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
   });
   mainWindow.loadFile("index.html");
 }
--- examples/electron-mediapipe-face/renderer.ts
+++ examples/electron-mediapipe-face/renderer.ts
@@ -1,1 +1,29 @@
-console.log("hello");
+import { Interpreter } from "node-tflite";
+
+const init = async () => {
+  const video = document.createElement("video");
+  video.width = 640;
+  video.height = 480;
+  const stream = await navigator.mediaDevices.getUserMedia({
+    audio: false,
+    video: {
+      width: { ideal: video.width },
+      height: { ideal: video.height },
+    },
+  });
+  video.srcObject = stream;
+  video.play();
+
+  const videoCanvas = document.createElement("canvas");
+  videoCanvas.width = video.width;
+  videoCanvas.height = video.height;
+  const context = videoCanvas.getContext("2d")!;
+
+  const updateCanvas = () => {
+    context.drawImage(video, 0, 0);
+    requestAnimationFrame(updateCanvas);
+  };
+  updateCanvas();
+};
+
+init();
