a05725476564f77c52d2bdb39eebbec9e6f8f748
Ryohei Ikegami <iofg2100@gmail.com>
2020-05-19 19:20:06 +0900

    Refactor

--- examples/electron-mediapipe-face/renderer.ts
+++ examples/electron-mediapipe-face/renderer.ts
@@ -2,52 +2,48 @@ import { Interpreter } from "node-tflite";
 import fs from "fs";
 import path from "path";
 
-function canvasToRGBFloat(context: CanvasRenderingContext2D) {
-  const { width, height } = context.canvas;
-  const data = context.getImageData(0, 0, width, height);
+async function init() {
+  const video = document.createElement("video");
+  video.width = 640;
+  video.height = 480;
+  const stream = await navigator.mediaDevices.getUserMedia({
+    audio: false,
+    video: {
+      width: { ideal: video.width },
+      height: { ideal: video.height },
+    },
+  });
+  video.srcObject = stream;
+  video.play();
 
-  const rgbFloat = new Float32Array(width * height * 3);
+  const faceDetector = new FaceDetector();
 
-  for (let i = 0; i < width * height; ++i) {
-    rgbFloat[i * 3] = data.data[i * 4] / 255;
-    rgbFloat[i * 3 + 1] = data.data[i * 4 + 1] / 255;
-    rgbFloat[i * 3 + 2] = data.data[i * 4 + 2] / 255;
-  }
+  const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+  canvas.width = 640;
+  canvas.height = 480;
+  const context = canvas.getContext("2d")!;
+  context.strokeStyle = "red";
+  context.lineWidth = 2;
 
-  return rgbFloat;
-}
+  const animate = () => {
+    const rect = faceDetector.detect(video);
 
-function sigmoid(x: number) {
-  return 1 / (1 + Math.exp(-x));
-}
-
-function generateAnchors(width: number, height: number): [number, number][] {
-  const outputSpec = {
-    strides: [8, 16] as const,
-    anchors: [2, 6] as const,
-  };
-
-  const anchors: [number, number][] = [];
-  for (let i = 0; i < outputSpec.strides.length; i++) {
-    const stride = outputSpec.strides[i];
-    const gridRows = Math.floor((height + stride - 1) / stride);
-    const gridCols = Math.floor((width + stride - 1) / stride);
-    const anchorsNum = outputSpec.anchors[i];
-
-    for (let gridY = 0; gridY < gridRows; gridY++) {
-      const anchorY = stride * (gridY + 0.5);
-
-      for (let gridX = 0; gridX < gridCols; gridX++) {
-        const anchorX = stride * (gridX + 0.5);
-        for (let n = 0; n < anchorsNum; n++) {
-          anchors.push([anchorX, anchorY]);
-        }
-      }
+    context.drawImage(video, 0, 0);
+    if (rect) {
+      context.strokeRect(
+        rect[0],
+        rect[1],
+        rect[2] - rect[0],
+        rect[3] - rect[1]
+      );
     }
-  }
 
-  return anchors;
+    requestAnimationFrame(animate);
+  };
+  animate();
 }
+
+init();
 
 type Box = [number, number, number, number]; // left, top, right, bottom
 
@@ -56,7 +52,7 @@ class FaceDetector {
   private inputCanvas: HTMLCanvasElement;
   private inputContext: CanvasRenderingContext2D;
   private interpreter: Interpreter;
-  private anchors = generateAnchors(this.inputSize, this.inputSize);
+  private anchors = this.generateAnchors(this.inputSize, this.inputSize);
 
   constructor() {
     this.inputCanvas = document.createElement("canvas");
@@ -115,47 +111,51 @@ class FaceDetector {
       (bottom / this.inputSize) * (input.height as number),
     ];
   }
-}
 
-const init = async () => {
-  const video = document.createElement("video");
-  video.width = 640;
-  video.height = 480;
-  const stream = await navigator.mediaDevices.getUserMedia({
-    audio: false,
-    video: {
-      width: { ideal: video.width },
-      height: { ideal: video.height },
-    },
-  });
-  video.srcObject = stream;
-  video.play();
+  private generateAnchors(width: number, height: number): [number, number][] {
+    const outputSpec = {
+      strides: [8, 16] as const,
+      anchors: [2, 6] as const,
+    };
 
-  const faceDetector = new FaceDetector();
+    const anchors: [number, number][] = [];
+    for (let i = 0; i < outputSpec.strides.length; i++) {
+      const stride = outputSpec.strides[i];
+      const gridRows = Math.floor((height + stride - 1) / stride);
+      const gridCols = Math.floor((width + stride - 1) / stride);
+      const anchorsNum = outputSpec.anchors[i];
 
-  const canvas = document.getElementById("canvas") as HTMLCanvasElement;
-  canvas.width = 640;
-  canvas.height = 480;
-  const context = canvas.getContext("2d")!;
-  context.strokeStyle = "red";
-  context.lineWidth = 2;
+      for (let gridY = 0; gridY < gridRows; gridY++) {
+        const anchorY = stride * (gridY + 0.5);
 
-  const animate = () => {
-    const rect = faceDetector.detect(video);
-
-    context.drawImage(video, 0, 0);
-    if (rect) {
-      context.strokeRect(
-        rect[0],
-        rect[1],
-        rect[2] - rect[0],
-        rect[3] - rect[1]
-      );
+        for (let gridX = 0; gridX < gridCols; gridX++) {
+          const anchorX = stride * (gridX + 0.5);
+          for (let n = 0; n < anchorsNum; n++) {
+            anchors.push([anchorX, anchorY]);
+          }
+        }
+      }
     }
 
-    requestAnimationFrame(animate);
-  };
-  animate();
-};
+    return anchors;
+  }
+}
 
-init();
+function canvasToRGBFloat(context: CanvasRenderingContext2D) {
+  const { width, height } = context.canvas;
+  const data = context.getImageData(0, 0, width, height);
+
+  const rgbFloat = new Float32Array(width * height * 3);
+
+  for (let i = 0; i < width * height; ++i) {
+    rgbFloat[i * 3] = data.data[i * 4] / 255;
+    rgbFloat[i * 3 + 1] = data.data[i * 4 + 1] / 255;
+    rgbFloat[i * 3 + 2] = data.data[i * 4 + 2] / 255;
+  }
+
+  return rgbFloat;
+}
+
+function sigmoid(x: number) {
+  return 1 / (1 + Math.exp(-x));
+}
