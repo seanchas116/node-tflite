b9c3cfa6f68683de3ae6791acabcdcb8e2d33c32
Ryohei Ikegami <iofg2100@gmail.com>
2020-05-19 14:35:21 +0900

    Init example project

---
+++ examples/electron-mediapipe-face/index.ts
@@ -0,0 +1,17 @@
+import { app, BrowserWindow } from "electron";
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+  mainWindow.loadFile("index.html");
+}
+
+app.whenReady().then(() => {
+  createWindow();
+});
+
+app.on("window-all-closed", () => {
+  app.quit();
+});
